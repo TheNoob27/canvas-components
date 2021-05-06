7ba38c6c94231df5f7cb2fe451f84cdbac95edb1
TheNoob27 <bwsuperweevil@gmail.com>
2021-05-06 18:01:52 +0100

    add css names, constants and util

---
+++ src/util/constants.ts
@@ -0,0 +1,11 @@
+import cssProperties from "../cssProperties"
+
+const Constants = {
+  cssProperties,
+  supportedProperties: [
+    "backgroundColor",
+    "display"
+  ]
+}
+
+export default Constants
